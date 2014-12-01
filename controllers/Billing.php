761395a740ac3fbfaaa9d021f0023042cb9edca9
Orest Budzhak <orest@dataedu.com>
2014-12-01 18:06:52 +0200

    update billing codes

--- controllers/Billing.php
+++ controllers/Billing.php
@@ -16,8 +16,7 @@ class Billing extends BaseController
             $dataTableRequest = new \System\DataTableRequest($this->di);
 
             $data = $billingModel->getDataTableData(
-                    $this->auth['id'], 
-                    $dataTableRequest->buildResult(array('active'))
+                    $this->auth['id'], $dataTableRequest->buildResult(array('active'))
             );
             $this->checkDisplayLength($dataTableRequest->getDisplayLength());
             $this->makeJSONResponse($data);
@@ -43,9 +42,13 @@ class Billing extends BaseController
 
         $list = $devicesManager->getUserUnAssignedDevicesList($this->auth['id']);
 
-        if ($this->getRequest()->isPost()) {
-            $device = $this->getRequest()->post('device');
-            if ($device == null || !in_array($device, array_keys($list))) {
+        if (!count($list)) {
+            $this->redirect($this->di['router']->getRouteUrl('billingAddDevice') . '?license=' . $license);
+        }
+
+        $device = $this->getRequest()->get('device');
+        if ($device !== null) {
+            if (!in_array($device, array_keys($list))) {
                 $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Invalid device!'));
             }
 
@@ -56,25 +59,69 @@ class Billing extends BaseController
 
         $this->view->title = $this->di->getTranslator()->_('Assign Device');
         $this->view->devices = $list;
+        $this->view->license = $license;
         $this->setView('billing/assignDevice.htm');
     }
-    
+
     public function addDeviceAction()
     {
         $this->view->title = $this->di->getTranslator()->_('New Device');
 
+        $license = $this->getRequest()->get('license');
+
         $devicesManager = new DevicesManager($this->di['db']);
-        $devicesManager->setRedisConfig($this->di['config']['redis']);
+
+        if ($this->getRequest()->hasGet('code')) {
+            $this->completeAddDevice($devicesManager);
+        }
+
+        if ($license != null &&
+                !$devicesManager->isUserLicenseAvailable($license, $this->auth['id'])) {
+
+            $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Invalid license!'));
+            $this->redirect($this->di['router']->getRouteUrl('billing'));
+        }
 
         try {
-            $this->view->code = $devicesManager->getUserDeviceAddCode($this->auth['id']);
+            $code = $devicesManager->getUserDeviceAddCode($this->auth['id'], $license);
         } catch (CS\Devices\Manager\DeviceCodeGenerationException $e) {
             $this->di->getFlashMessages()->add(FlashMessages::ERROR, "Error during add device! Pleace try again later!");
             $this->di['logger']->addCritical("Device code generation failed!");
-            $this->view->code = false;
+            $this->redirect($this->di['router']->getRouteUrl('billing'));
         }
 
+        $this->view->code = $code;
+        $this->view->displayCode = str_pad($code, 4, '0', STR_PAD_LEFT);
+
         $this->setView('billing/addDevice.htm');
+    }
+
+    private function completeAddDevice(DevicesManager $devicesManager)
+    {
+        $code = $this->getRequest()->get('code');
+        $info = $devicesManager->getUserAddCodeInfo($this->auth['id'], $code);
+
+        if ($info === false) {
+            $this->di->getFlashMessages()->add(FlashMessages::ERROR, "Code not found!");
+            $this->redirect($this->di['router']->getRouteUrl('billing'));
+        }
+
+        if ($info['assigned']) {
+            $this->di->getFlashMessages()->add(FlashMessages::SUCCESS, "Your device successfully added!");
+            $this->redirect($this->di['router']->getRouteUrl('billing'));
+        }
+        
+        if ($info['expired']) {
+            $this->di->getFlashMessages()->add(FlashMessages::INFO, "Code was expired. We've generated new code for you. Please enter it on mobile phone.");
+        } else {
+            $this->di->getFlashMessages()->add(FlashMessages::INFO, "It looks you haven't entered code on mobile yet. Please do it now.");
+        }
+
+        if ($info['license_id'] !== null) {
+            $this->redirect($this->di['router']->getRouteUrl('billingAddDevice') . '?license=' . $info['license_id']);
+        } else {
+            $this->redirect($this->di['router']->getRouteUrl('billingAddDevice'));
+        }
     }
 
 }
