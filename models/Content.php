1bbb719f4aa5d4934bf20bc913aa94860ae03548
Orest Budzhak <orest@dataedu.com>
2014-12-06 16:27:54 +0200

    fix templates locale exception

--- models/Content.php
+++ models/Content.php
@@ -5,7 +5,7 @@ namespace Models;
 class Content extends \System\Model {
 
     public function getTemplatePath($template) {
-        return 'content/' . $this->di['locale'] . '/' . $template;
+        return 'content/' . $this->di->getTranslator()->getLocale() . '/' . $template;
     }
     
 }
