1ce72cdbc6b6c09612fa0ac5f6359e444ed40032
Orest Budzhak <orest@dataedu.com>
2015-02-20 10:21:37 +0200

    add license upgrade

--- controllers/Billing.php
+++ controllers/Billing.php
@@ -34,81 +34,78 @@ class Billing extends BaseController
 
         $this->setView('billing/index.htm');
     }
-    
+
     public function addICloudDeviceAction()
     {
         $devicesManager = new DevicesManager($this->di['db']);
         $licenseRecord = new LicenseRecord($this->di['db']);
 
         try {
-           
+
             $licenseRecord->load($this->getRequest()->get('license'));
-            if(    $licenseRecord->getUserId() != $this->auth['id']
-                || $licenseRecord->getStatus() != LicenseRecord::STATUS_AVAILABLE
-                //todo $licenseRecord->getProduct()->getGroup()
-                || $licenseRecord->getOrderProduct()->getProduct()->getGroup() != 'premium')
-                    throw new LicenseNotFoundException;
-            if($_POST){
+            if ($licenseRecord->getUserId() != $this->auth['id'] || $licenseRecord->getStatus() != LicenseRecord::STATUS_AVAILABLE
+                    //todo $licenseRecord->getProduct()->getGroup()
+                    || $licenseRecord->getOrderProduct()->getProduct()->getGroup() != 'premium')
+                throw new LicenseNotFoundException;
+            if ($_POST) {
                 $iCloud = new ICloudBackup($_POST['email'], $_POST['password']);
                 $devices = $iCloud->getDevices();
 
-                if(empty($devices)){
+                if (empty($devices)) {
                     $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Account has no devices. Please try another'));
                     $this->redirect($this->di['router']->getRouteUrl('billingAddICloudDevice'));
+                } else
+                    $devices = $devicesManager->iCloudMergeWithLocalInfo($this->auth['id'], $devices);
 
-                } else $devices = $devicesManager->iCloudMergeWithLocalInfo($this->auth['id'], $devices);
+                if (isset($_POST['devHash']) && !empty($_POST['devHash'])) {
 
-                if(isset($_POST['devHash']) && !empty($_POST['devHash'])){
+                    foreach ($devices as &$device) {
+                        if ($device['SerialNumber'] === $_POST['devHash'] && !$device['added']) {
 
-                    foreach($devices as &$device) {
-                        if($device['SerialNumber'] === $_POST['devHash'] && !$device['added']){
-                            
-                            $devicesManager->setDeviceDbConfigGenerator(function($devId){
+                            $devicesManager->setDeviceDbConfigGenerator(function($devId) {
                                 if ($this->di['config']['environment'] == 'production') {
                                     return GlobalSettings::getDeviceDatabaseConfig($devId);
-
-                                } else return $this->di['config']['dataDb'];
+                                } else
+                                    return $this->di['config']['dataDb'];
                             });
 
                             $devicesManager
-                                ->setUserId($this->auth['id'])
-                                ->setLicense($licenseRecord)
-                                ->setDeviceUniqueId($device['SerialNumber'])
-                                ->setAppleId($_POST['email'])
-                                ->setApplePassword($_POST['password'])
-                                ->setDeviceHash($device['backupUDID'])
-                                ->setName($device['DeviceName'])
-                                ->setModel($device['MarketingName'])
-                                ->setOsVer($device['ProductVersion'])
-                                ->setLastBackup($device['LastModified'])
-                                ->setQuotaUsed($device['QuotaUsed'])
-                                ->setAfterSave(function() use ($devicesManager){
-                                    $this->di['flashMessages']->add(FlashMessages::SUCCESS, $this->di['t']->_(
-                                        'New device added'
-                                    ));
+                                    ->setUserId($this->auth['id'])
+                                    ->setLicense($licenseRecord)
+                                    ->setDeviceUniqueId($device['SerialNumber'])
+                                    ->setAppleId($_POST['email'])
+                                    ->setApplePassword($_POST['password'])
+                                    ->setDeviceHash($device['backupUDID'])
+                                    ->setName($device['DeviceName'])
+                                    ->setModel($device['MarketingName'])
+                                    ->setOsVer($device['ProductVersion'])
+                                    ->setLastBackup($device['LastModified'])
+                                    ->setQuotaUsed($device['QuotaUsed'])
+                                    ->setAfterSave(function() use ($devicesManager) {
+                                        $this->di['flashMessages']->add(FlashMessages::SUCCESS, $this->di['t']->_(
+                                                        'New device added'
+                                        ));
 
-                                    $userNotes = new UsersNotes($this->di['db']);
-                                    $userNotes->addSystemNote($this->auth['id'], UsersNotes::TYPE_SYSTEM, null, null,
-                                        "New device added {$devicesManager->getProcessedDevice()->getName()} " . json_encode(array(
-                                            'dev_id' => $devicesManager->getProcessedDevice()->getUniqueId()
+                                        $userNotes = new UsersNotes($this->di['db']);
+                                        $userNotes->addSystemNote($this->auth['id'], UsersNotes::TYPE_SYSTEM, null, null, "New device added {$devicesManager->getProcessedDevice()->getName()} " . json_encode(array(
+                                                    'dev_id' => $devicesManager->getProcessedDevice()->getUniqueId()
                                         )));
-                                    $userNotes->addSystemNote($this->auth['id'], UsersNotes::TYPE_SYSTEM, null, null,
-                                        "Assign {$devicesManager->getLicense()->getOrderProduct()->getProduct()->getName()} to device {$devicesManager->getProcessedDevice()->getName()} " . json_encode(array(
-                                            'device_id' => $devicesManager->getProcessedDevice()->getId(),
-                                            'license_id' => $devicesManager->getLicense()->getId()
-                                        ))
-                                    );
+                                        $userNotes->addSystemNote($this->auth['id'], UsersNotes::TYPE_SYSTEM, null, null, "Assign {$devicesManager->getLicense()->getOrderProduct()->getProduct()->getName()} to device {$devicesManager->getProcessedDevice()->getName()} " . json_encode(array(
+                                                    'device_id' => $devicesManager->getProcessedDevice()->getId(),
+                                                    'license_id' => $devicesManager->getLicense()->getId()
+                                                ))
+                                        );
 
-                                    $queueManager = new \CS\Queue\Manager($this->di['queueClient']);
+                                        $queueManager = new \CS\Queue\Manager($this->di['queueClient']);
 
-                                    if($queueManager->addDownloadTask($devicesManager->getICloudDevice())){
-                                        $devicesManager->getICloudDevice()->setProcessing(1);
+                                        if ($queueManager->addDownloadTask($devicesManager->getICloudDevice())) {
+                                            $devicesManager->getICloudDevice()->setProcessing(1);
+                                        } else
+                                            $devicesManager->getICloudDevice()->setLastError($queueManager->getError());
 
-                                    } else $devicesManager->getICloudDevice()->setLastError($queueManager->getError());
-
-                                    $devicesManager->getICloudDevice()->save();
-                                })
-                                ->addICloudDevice();
+                                        $devicesManager->getICloudDevice()->save();
+                                    })
+                                    ->addICloudDevice();
 
                             $this->redirect($this->di['router']->getRouteUrl('billing'));
                             break;
@@ -124,15 +121,12 @@ class Billing extends BaseController
                 $this->setView('billing/addICloudDevice.htm');
                 return;
             }
-
         } catch (LicenseNotFoundException $e) {
             $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Invalid license!'));
             $this->redirect($this->di['router']->getRouteUrl('billing'));
-
         } catch (\CS\ICloud\AuthorizationException $e) {
             $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Wrong Apple ID or password'));
             $this->redirect($this->di['router']->getRouteUrl('billingAddICloudDevice') . '?license=' . $this->getRequest()->get('license'));
-
         } catch (\Exception $e) {
             $this->di['logger']->addCritical(get_class($e) . " {$e->getMessage()} {$e->getFile()}[{$e->getLine()}] " . p($e));
             $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Unexpected Error. Please try later or contact us!'));
@@ -149,24 +143,22 @@ class Billing extends BaseController
         $licenseRecord = new LicenseRecord($this->di['db']);
         $license = $this->getRequest()->get('license');
         $this->view->iCloudLicenseAvailable = false;
-        
+
         try {
             $licenseRecord->load($license);
-            if(    $licenseRecord->getUserId() != $this->auth['id'] 
-                || $licenseRecord->getStatus() !=  LicenseRecord::STATUS_AVAILABLE )
-                    throw new LicenseNotFoundException;
-            
+            if ($licenseRecord->getUserId() != $this->auth['id'] || $licenseRecord->getStatus() != LicenseRecord::STATUS_AVAILABLE) {
+                throw new LicenseNotFoundException;
+            }
         } catch (LicenseNotFoundException $e) {
             $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Invalid license!'));
             $this->redirect($this->di['router']->getRouteUrl('billing'));
         }
 
         //todo $licenseRecord->getProduct()->getGroup()
-        if(($this->auth['id'] == 78 || $this->auth['id'] == 9) 
-            && $licenseRecord->getOrderProduct()->getProduct()->getGroup() == 'premium')
-                $this->view->iCloudLicenseAvailable = true;
+        if (($this->auth['id'] == 78 || $this->auth['id'] == 9) && $licenseRecord->getOrderProduct()->getProduct()->getGroup() == 'premium')
+            $this->view->iCloudLicenseAvailable = true;
 
-        $list = $devicesManager->getUserUnAssignedDevicesList($this->auth['id']);
+        $list = $devicesManager->getDevicesToAssign($this->auth['id']);
 
         if (!count($list) && !$this->view->iCloudLicenseAvailable) {
             $this->redirect($this->di['router']->getRouteUrl('billingAddDevice') . '?license=' . $license);
@@ -178,22 +170,28 @@ class Billing extends BaseController
                 $this->di['flashMessages']->add(FlashMessages::ERROR, $this->di['t']->_('Invalid device!'));
             }
 
-            $devicesManager->assignLicenseToDevice($license, $device);
-            $this->di['flashMessages']->add(FlashMessages::SUCCESS, $this->di['t']->_('Device successfully assigned to your license!'));
+            if (!$this->getRequest()->hasGet('confirm') && $devicesManager->hasDevicePackageLicense($device)) {
+                $this->view->deviceConfirm = $device;
+            } else {
+                $devicesManager->removeDeviceLicenses($device);
+                
+                $devicesManager->assignLicenseToDevice($license, $device);
+                $this->di['flashMessages']->add(FlashMessages::SUCCESS, $this->di['t']->_('Device successfully assigned to your license!'));
 
-            $licInfo = (new \Models\Billing($this->di))
-                    ->getLicenseDeviceInfo($license);
+                $licInfo = (new \Models\Billing($this->di))
+                        ->getLicenseDeviceInfo($license);
 
-            if ($licInfo !== false) {
-                (new \Models\Users($this->di))
-                        ->addSystemNote($this->auth['id'], "Assign {$licInfo['product_name']} to device {$licInfo['device_name']} " . json_encode(array(
-                                    'device_id' => $licInfo['dev_id'],
-                                    'license_id' => $licInfo['license_id']
-                                ))
-                );
+                if ($licInfo !== false) {
+                    (new \Models\Users($this->di))
+                            ->addSystemNote($this->auth['id'], "Assign {$licInfo['product_name']} to device {$licInfo['device_name']} " . json_encode(array(
+                                        'device_id' => $licInfo['dev_id'],
+                                        'license_id' => $licInfo['license_id']
+                                    ))
+                    );
+                }
+
+                $this->redirect($this->di['router']->getRouteUrl('billing'));
             }
-
-            $this->redirect($this->di['router']->getRouteUrl('billing'));
         }
 
         $this->view->title = $this->di->getTranslator()->_('Assign Device');
