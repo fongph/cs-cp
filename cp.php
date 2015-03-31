6506532ffd39476219bef705703f8685eee3f652
Orest Budzhak <orest@dataedu.com>
2015-03-31 12:13:02 +0300

    add update-demo-user command

--- cp.php
+++ cp.php
@@ -109,7 +109,7 @@ $console->register('load-demo-user')
                 $output->writeln("<error>Demo user not found!</error>");
                 return;
             }
-            
+
             if (file_put_contents(ROOT_PATH . 'demoUserData.php', '<?php return ' . var_export_min($userData, true) . ';', LOCK_EX) == false) {
                 $output->writeln("<error>File write error!</error>");
                 return;
@@ -117,7 +117,88 @@ $console->register('load-demo-user')
 
             $output->writeln('User data successfully exported to file!');
         });
-        
+
+$console->register('update-demo-user')
+        ->setDescription('Update demo user data time')
+        ->setDefinition(array(
+            new InputArgument('days', InputArgument::REQUIRED, 'The number of days to increase')
+        ))
+        ->setCode(function (InputInterface $input, OutputInterface $output) {
+            $days = $input->getArgument('days');
+
+            if (!is_file(ROOT_PATH . 'build.php')) {
+                $output->writeln("<error>build.php file file not found!</error>");
+                return;
+            }
+
+            $config = include ROOT_PATH . 'build.php';
+
+            if ($config['demo'] == 0) {
+                $output->writeln("<error>Demo user id not defined for this build!</error>");
+                return;
+            }
+            
+            $demoUserId = $config['demo'];
+
+            $di = new System\DI();
+            $di->set('config', $config);
+
+            require ROOT_PATH . 'bootstrap.php';
+
+            $logger = new Monolog\Logger('logger');
+            $logger->pushHandler(new Monolog\Handler\StreamHandler(ROOT_PATH . 'logs/demo.log', Monolog\Logger::INFO));
+            
+            $userDevices = $di['db']->query("SELECT `id` FROM `devices` WHERE `user_id` = {$demoUserId}")->fetchAll(\PDO::FETCH_COLUMN);
+            
+            /**
+             * @todo Update to use multiple data databases
+             */
+            
+            $pdo = $di['dataDb'];
+            
+            try {
+                $value = $days * 3600 * 24;
+                $devicesExpression = '`dev_id` IN (' . implode(',', $userDevices) . ')';
+                
+                $pdo->beginTransaction();
+                $pdo->exec("UPDATE `applications_timelines` SET `start` = `start` + {$value}, `finish` = `finish` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `browser_history` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `calendar_events` SET `start` = `start` + {$value}, `end` = `end` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `call_log` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `emails` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `facebook_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `geo_events` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `instagram_comments` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `instagram_posts` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `keylogger` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `kik_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `photos` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `skype_calls` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `skype_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `sms_log` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `viber_calls` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `viber_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `video` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `vk_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                $pdo->exec("UPDATE `whatsapp_messages` SET `timestamp` = `timestamp` + {$value} WHERE {$devicesExpression}");
+                
+            } catch (\Exception $exception) {
+                $logger->addError(sprintf(
+                                'Exception during updatting demo user data - %s: "%s" at %s line %s', get_class($exception), $exception->getMessage(), $exception->getFile(), $exception->getLine()
+                ));
+                $pdo->rollBack();
+                $output->writeln("<error>Exception during updatting demo user data</error>");
+                return;
+            }
+            
+            $pdo->commit();
+
+            $message = "User data successfully updated by $days days!";
+
+            $logger->addInfo($message);
+            $output->writeln($message);
+        });
+
 $console->register('add-user')
         ->setDefinition(array(
             new InputArgument('email', InputArgument::REQUIRED, 'User email'),
@@ -141,10 +222,10 @@ $console->register('add-user')
 
             $usersManager = new CS\Users\UsersManager($di->get('db'));
             $usersManager->getUser()
-                ->setLogin($email)
-                ->setSiteId($di['config']['site'])
-                ->setPassword($usersManager->getPasswordHash($input->getArgument('password')))
-                ->save();
+            ->setLogin($email)
+            ->setSiteId($di['config']['site'])
+            ->setPassword($usersManager->getPasswordHash($input->getArgument('password')))
+            ->save();
 
             $output->writeln(sprintf('Record created for user <info>%s</info>', $email));
         });
